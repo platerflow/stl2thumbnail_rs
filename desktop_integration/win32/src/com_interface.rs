c0bc5e685c73ba04ddd1214d1bd98e380726cbfe
krepa098 <krepa098@gmail.com>
2021-01-31 15:09:53 +0100

    update to com=0.4

--- desktop_integration/win32/src/com_interface.rs
+++ desktop_integration/win32/src/com_interface.rs
@@ -1,5 +1,4 @@
-use com::ComPtr;
-use com::{com_interface, interfaces::iunknown::IUnknown, sys::HRESULT};
+use com::{interfaces::iunknown::IUnknown, sys::HRESULT};
 
 use winapi::shared::minwindef::{DWORD, PINT, UINT, ULONG};
 use winapi::shared::windef::HBITMAP;
@@ -8,37 +7,39 @@ use winapi::um::winnt::{LONGLONG, ULONGLONG};
 
 use std::ffi::c_void;
 
-#[com_interface("0000000c-0000-0000-C000-000000000046")]
-pub trait IStream: ISequentialStream {
-    unsafe fn clone(&self, ppstm: ComPtr<dyn IStream>) -> HRESULT;
-    unsafe fn commit(&self, commit_flags: DWORD) -> HRESULT;
-    unsafe fn copy_to(
-        &self,
-        ppstm: ComPtr<dyn IStream>,
-        cb: ULONGLONG,
-        pcb_read: *mut ULONGLONG,
-        pcb_written: *mut ULONGLONG,
-    ) -> HRESULT;
-    unsafe fn lock_region(&self, lib_offset: ULONGLONG, cb: ULONGLONG, lock_type: DWORD) -> HRESULT;
-    unsafe fn revert(&self) -> HRESULT;
-    unsafe fn seek(&self, dlib_move: LONGLONG, dw_origin: DWORD, plib_new_position: *mut ULONGLONG) -> HRESULT;
-    unsafe fn set_size(&self, lib_new_size: ULONGLONG) -> HRESULT;
-    unsafe fn stat(&self, pstatstg: *mut STATSTG, grf_stat_flag: DWORD) -> HRESULT;
-    unsafe fn unlock_region(&self, lib_offset: ULONGLONG, cb: ULONGLONG, lock_type: DWORD) -> HRESULT;
-}
+com::interfaces! {
+    #[uuid("0000000c-0000-0000-C000-000000000046")]
+    pub unsafe interface IStream: ISequentialStream {
+        pub fn clone(&self, ppstm: IStream) -> HRESULT;
+        pub fn commit(&self, commit_flags: DWORD) -> HRESULT;
+        pub fn copy_to(
+            &self,
+            ppstm: IStream,
+            cb: ULONGLONG,
+            pcb_read: *mut ULONGLONG,
+            pcb_written: *mut ULONGLONG,
+        ) -> HRESULT;
+        pub fn lock_region(&self, lib_offset: ULONGLONG, cb: ULONGLONG, lock_type: DWORD) -> HRESULT;
+        pub fn revert(&self) -> HRESULT;
+        pub fn seek(&self, dlib_move: LONGLONG, dw_origin: DWORD, plib_new_position: *mut ULONGLONG) -> HRESULT;
+        pub fn set_size(&self, lib_new_size: ULONGLONG) -> HRESULT;
+        pub fn stat(&self, pstatstg: *mut STATSTG, grf_stat_flag: DWORD) -> HRESULT;
+        pub fn unlock_region(&self, lib_offset: ULONGLONG, cb: ULONGLONG, lock_type: DWORD) -> HRESULT;
+    }
 
-#[com_interface("14AD64D8-F24A-402F-A5EF-FA4E16AA4ED4")]
-pub trait ISequentialStream: IUnknown {
-    unsafe fn read(&self, pv: *mut c_void, cb: ULONG, pcb_read: *mut ULONG) -> HRESULT;
-    unsafe fn write(&self, pv: *mut c_void, cb: ULONG, pcb_written: *mut ULONG) -> HRESULT;
-}
+    #[uuid("14AD64D8-F24A-402F-A5EF-FA4E16AA4ED4")]
+    pub unsafe interface ISequentialStream: IUnknown {
+        pub fn read(&self, pv: *mut c_void, cb: ULONG, pcb_read: *mut ULONG) -> HRESULT;
+        pub fn write(&self, pv: *mut c_void, cb: ULONG, pcb_written: *mut ULONG) -> HRESULT;
+    }
 
-#[com_interface("E357FCCD-A995-4576-B01F-234630154E96")]
-pub trait IThumbnailProvider: IUnknown {
-    unsafe fn get_thumbnail(&self, cx: UINT, phbmp: *mut HBITMAP, pdw_alpha: PINT) -> HRESULT;
-}
+    #[uuid("E357FCCD-A995-4576-B01F-234630154E96")]
+    pub unsafe interface IThumbnailProvider: IUnknown {
+        pub fn get_thumbnail(&self, cx: UINT, phbmp: *mut HBITMAP, pdw_alpha: PINT) -> HRESULT;
+    }
 
-#[com_interface("b824b49d-22ac-4161-ac8a-9916e8fa3f7f")]
-pub trait IInitializeWithStream: IUnknown {
-    unsafe fn initialize(&self, pstream: ComPtr<dyn IStream>, grf_mode: DWORD) -> HRESULT;
+    #[uuid("b824b49d-22ac-4161-ac8a-9916e8fa3f7f")]
+    pub unsafe interface IInitializeWithStream: IUnknown {
+        pub fn initialize(&self, pstream: IStream, grf_mode: DWORD) -> HRESULT;
+    }
 }
--- desktop_integration/win32/src/generator.rs
+++ desktop_integration/win32/src/generator.rs
@@ -1,108 +1,94 @@
-use crate::com_interface::{IInitializeWithStream, ISequentialStream, IStream, IThumbnailProvider};
-use com::co_class;
-use com::sys::{HRESULT, NOERROR};
-use com::ComPtr;
+use crate::com_interface::{IInitializeWithStream, IStream, IThumbnailProvider};
 
-use std::ptr::null_mut;
+use com::sys::{HRESULT, NOERROR};
+
 use winapi::shared::minwindef::{DWORD, PINT, UINT};
 use winapi::shared::windef::HBITMAP;
 use winapi::shared::wtypes::STATFLAG_NONAME;
 use winapi::um::objidlbase::STATSTG;
 use winapi::um::wingdi::CreateBitmap;
-use winapi::um::wingdi::BITMAP;
-use winapi::um::winnt::ULONGLONG;
 
 use std::cell::RefCell;
 use std::ffi::c_void;
-
 use std::io::Cursor;
+use std::ptr::null_mut;
 use std::time::Duration;
+
 use stl2thumbnail::parser::Parser;
 use stl2thumbnail::picture::Picture;
 use stl2thumbnail::rasterbackend::RasterBackend;
 
-#[co_class(implements(IThumbnailProvider, IInitializeWithStream))]
-pub struct WinSTLThumbnailGenerator {
-    data: RefCell<Vec<u8>>,
-}
-
-impl WinSTLThumbnailGenerator {
-    pub unsafe fn new() -> Box<Self> {
-        Self::allocate(RefCell::new(Vec::new()))
+com::class! {
+    pub class WinSTLThumbnailGenerator: IThumbnailProvider, IInitializeWithStream {
+        data: RefCell<Vec<u8>>, // will be initialized to Default::default()
     }
-}
 
-impl IThumbnailProvider for WinSTLThumbnailGenerator {
-    unsafe fn get_thumbnail(
-        &self,
-        cx: UINT,            // size in x & y dimension
-        phbmp: *mut HBITMAP, // data ptr
-        pdw_alpha: PINT,
-    ) -> com::sys::HRESULT {
-        *phbmp = null_mut();
-        *pdw_alpha = 0x0; // WTSAT_UNKNOWN
+    impl IThumbnailProvider for WinSTLThumbnailGenerator {
+        unsafe fn get_thumbnail(
+            &self,
+            cx: UINT,            // size in x & y dimension
+            phbmp: *mut HBITMAP, // data ptr
+            pdw_alpha: PINT,
+        ) -> com::sys::HRESULT {
+            *phbmp = null_mut();
+            *pdw_alpha = 0x0; // WTSAT_UNKNOWN
 
-        let slice = &self.data.borrow()[..];
-        let reader = Cursor::new(slice);
-        let mut parser = Parser::from_buf(reader, false).unwrap();
+            let slice = &self.data.borrow()[..];
+            let reader = Cursor::new(slice);
+            let mut parser = Parser::from_buf(reader, false).unwrap();
 
-        if let Ok(mesh) = parser.read_all() {
-            let mut backend = RasterBackend::new(cx as u32, cx as u32);
-            let (aabb, scale) = backend.fit_mesh_scale(&mesh);
-            backend.render_options.zoom = 1.05;
-            // Note: Icon cache sizes seem to be 16,32,48,96,256,768,1280,...
-            // 256 this is actually too small to be readable when it first appears,
-            // but 768 makes no sense either
-            backend.render_options.draw_size_hint = cx >= 256;
-            let pic = backend.render(&mesh, scale, &aabb, Some(Duration::from_secs(20)));
+            if let Ok(mesh) = parser.read_all() {
+                let mut backend = RasterBackend::new(cx as u32, cx as u32);
+                let (aabb, scale) = backend.fit_mesh_scale(&mesh);
+                backend.render_options.zoom = 1.05;
+                // Note: Icon cache sizes seem to be 16,32,48,96,256,768,1280,...
+                // 256 this is actually too small to be readable when it first appears,
+                // but 768 makes no sense either
+                backend.render_options.draw_size_hint = cx >= 256;
+                let pic = backend.render(&mesh, scale, &aabb, Some(Duration::from_secs(20)));
 
-            *phbmp = create_hbitmap_from_picture(&pic);
-            *pdw_alpha = 0x2; // WTSAT_ARGB
+                *phbmp = create_hbitmap_from_picture(&pic);
+                *pdw_alpha = 0x2; // WTSAT_ARGB
 
-            return NOERROR;
-        }
-
-        -1 // error
-
-        // **this works for testing**
-        // let mut pic = Picture::new(cx as usize, cx as usize);
-        // pic.test_pattern();
-        // *phbmp = create_hbitmap_from_picture(&pic);
-        // *pdw_alpha = 0x2; // WTSAT_ARGB
-
-        // return NOERROR
-    }
-}
-
-impl IInitializeWithStream for WinSTLThumbnailGenerator {
-    unsafe fn initialize(&self, pstream: ComPtr<dyn IStream>, grf_mode: DWORD) -> HRESULT {
-        if let Some(stream) = pstream.get_interface::<dyn IStream>() {
-            // figure out the length of the stream
-            let mut stat: STATSTG = std::mem::zeroed();
-
-            if stream.stat(&mut stat, STATFLAG_NONAME) != NOERROR {
-                return -2;
+                return NOERROR;
             }
 
-            let len = *stat.cbSize.QuadPart() as usize;
+            -1 // error
 
-            println!("Got stream of length {}", len);
+            // **this works for testing**
+            // let mut pic = Picture::new(cx as usize, cx as usize);
+            // pic.test_pattern();
+            // *phbmp = create_hbitmap_from_picture(&pic);
+            // *pdw_alpha = 0x2; // WTSAT_ARGB
 
-            // read the entire stream
-            self.data.replace(vec![0; len as usize]);
-            stream.read(
-                self.data.borrow_mut().as_mut_ptr() as *mut c_void,
-                len as u32,
-                std::ptr::null_mut(),
-            );
-
-            return NOERROR;
-        } else {
-            println!("Error: Invalid stream interface");
-            return -1;
+            // return NOERROR
         }
     }
-}
+    impl IInitializeWithStream for WinSTLThumbnailGenerator {
+        unsafe fn initialize(&self, pstream: IStream, _grf_mode: DWORD) -> HRESULT {
+              // figure out the length of the stream
+              let mut stat: STATSTG = std::mem::zeroed();
+
+                if pstream.stat(&mut stat, STATFLAG_NONAME) != NOERROR {
+                    return -2;
+                }
+
+                let len = *stat.cbSize.QuadPart() as usize;
+
+                println!("Got stream of length {}", len);
+
+                // read the entire stream
+                self.data.replace(vec![0; len as usize]);
+                pstream.read(
+                    self.data.borrow_mut().as_mut_ptr() as *mut c_void,
+                    len as u32,
+                    std::ptr::null_mut(),
+                );
+
+                return NOERROR;
+        }
+    }
+} // class
 
 fn create_hbitmap_from_picture(pic: &Picture) -> HBITMAP {
     let bgra_data = pic.to_bgra();
